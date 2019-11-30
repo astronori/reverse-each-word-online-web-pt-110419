4f32c81791049c3327c400c771700dafde65f1ef
kenneth gil <astronori@gmail.com>
2019-11-30 15:13:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 phrase = "Hello there, and how are you?"
 
-def reverse_each_word(phrase)
+def reverse_each_word("Hello there, and how are you?")
   phrase.each do |phrase|
     puts phrase.reverse
   end
